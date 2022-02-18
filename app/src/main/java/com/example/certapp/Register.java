94b4556a5e6d8b832a523ea27cb336c492ae5abe
bjeevanreddy <bjeevanreddy45@gmail.com>
2022-02-17 19:42:41 -0600

    added validations for password

--- app/src/main/java/com/example/certapp/Register.java
+++ app/src/main/java/com/example/certapp/Register.java
@@ -2,9 +2,12 @@ package com.example.certapp;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
@@ -18,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +29,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -53,7 +59,6 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
         mAuth = FirebaseAuth.getInstance();
         RootRef = FirebaseDatabase.getInstance().getReference();
         fStore = FirebaseFirestore.getInstance();
-
         etFirstName = findViewById(R.id.etFirstName);
         etLastName = findViewById(R.id.etLastName);
         etEmail = findViewById(R.id.etEmail);
@@ -154,72 +159,97 @@ public class Register extends AppCompatActivity implements AdapterView.OnItemSel
             {
                 etPassword.setError("Password Length must be >= 8");
             }
+            if(verifyPassword.isEmpty())
+            {
+                etVerifyPassword.setError("Please Enter a password");
+            }
+            if(verifyPassword.length() < 8)
+            {
+                etVerifyPassword.setError("Password Length must be >= 8");
+            }
             if(address.isEmpty())
             {
                 etAddress.setError("Please Enter your Full Name");
             }
+            if(password.equals(verifyPassword)){
 
 
-            mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-                            if (task.isSuccessful()) {
-                                System.out.println("Task is successful ");
-                                User user1 = new User(name,lname,qualification,phoneNumber,city,state,address,zipCode,password,verifyPassword,"none",email);
-                                String mUid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
-                                FirebaseDatabase.getInstance().getReference("usersDB")
-                                        .child(mUid)
-                                        .setValue(user1).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            System.out.println("Task is successful inside");
-                                            userID = mAuth.getCurrentUser().getUid();
-                                            RootRef.child("usersDB").child(userID).setValue("");
-                                            DocumentReference documentReference = fStore.collection("usersDB").document(userID);
-                                            Map<String,Object> user = new HashMap<>();
-                                            user.put("firstName",user1.getFirstName());
-                                            user.put("lastName",user1.getLastName());
-                                            user.put("emailAddress",user1.getEmailAddress());
-                                            user.put("contactNumber",user1.getContactNumber());
-                                            user.put("city",user1.getCity());
-                                            user.put("qualification",user1.getQualification());
-                                            user.put("state",user1.getState());
-                                            user.put("streetAddress",user1.getStreetAddress());
-                                            user.put("zipCode",user1.getZipCode());
-                                            user.put("password",user1.getPassword());
-                                            user.put("verifyPassword",user1.getVerifyPassword());
-                                            user.put("others",user1.getOthers());
-                                            user.put("createdAt","12/2002");
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                            documentReference.set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if(task.isSuccessful()) {
-                                                        Log.e("User details ", "User details have registered successfully ");
-                                                    }else
-                                                    {
-                                                        Log.e("User not registered ","User not registered in real time database ");
+                                if (task.isSuccessful()) {
+                                    System.out.println("Task is successful ");
+                                    User user1 = new User(name,lname,qualification,phoneNumber,city,state,address,zipCode,password,verifyPassword,"none",email);
+                                    String mUid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+                                    FirebaseDatabase.getInstance().getReference("usersDB")
+                                            .child(mUid)
+                                            .setValue(user1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @RequiresApi(api = Build.VERSION_CODES.O)
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                System.out.println("Task is successful inside");
+                                                userID = mAuth.getCurrentUser().getUid();
+                                                RootRef.child("usersDB").child(userID).setValue("");
+                                                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                                                LocalDateTime now = LocalDateTime.now();
+                                                DocumentReference documentReference = fStore.collection("usersDB").document(userID);
+                                                Map<String,Object> user = new HashMap<>();
+                                                user.put("firstName",user1.getFirstName());
+                                                user.put("lastName",user1.getLastName());
+                                                user.put("emailAddress",user1.getEmailAddress());
+                                                user.put("contactNumber",user1.getContactNumber());
+                                                user.put("city",user1.getCity());
+                                                user.put("qualification",user1.getQualification());
+                                                user.put("state",user1.getState());
+                                                user.put("streetAddress",user1.getStreetAddress());
+                                                user.put("zipCode",user1.getZipCode());
+                                                user.put("password",user1.getPassword());
+                                                user.put("verifyPassword",user1.getVerifyPassword());
+                                                user.put("others",user1.getOthers());
+                                                user.put("createdAt",dtf.format(now));
+
+                                                documentReference.set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if(task.isSuccessful()) {
+                                                            Log.e("User details ", "User details have registered successfully ");
+                                                        }else
+                                                        {
+                                                            Log.e("User not registered ","User not registered in real time database ");
+                                                        }
                                                     }
-                                                }
-                                            });
-                                            Toast.makeText(Register.this, "User have registered Successfully", Toast.LENGTH_LONG).show();
-                                            Intent intent = new Intent(Register.this, MainActivity.class);
-                                            startActivity(intent);
-                                        } else {
-                                            Log.e("Registration", "Registration not in task ");
-                                            Toast.makeText(Register.this, "User registration was not successful", Toast.LENGTH_LONG).show();
+                                                });
+                                                Toast.makeText(Register.this, "User have registered Successfully", Toast.LENGTH_LONG).show();
+                                                Intent intent = new Intent(Register.this, MainActivity.class);
+                                                startActivity(intent);
+                                            } else {
+                                                Log.e("Registration", "Registration not in task ");
+                                                Toast.makeText(Register.this, "User registration was not successful", Toast.LENGTH_LONG).show();
+                                            }
                                         }
-                                    }
-                                });
-                            } else {
-                                Log.d("in Task ", "Not registered", task.getException());
-                                Toast.makeText(Register.this, "User registration was not successful In database Task", Toast.LENGTH_LONG).show();
+                                    });
+                                } else {
+                                    Log.d("in Task ", "Not registered", task.getException());
+                                    Toast.makeText(Register.this, "User registration was not successful In database Task", Toast.LENGTH_LONG).show();
+                                }
                             }
-                        }
-                    });
+                        });
+            }
+            else
+            {
+                Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Passwords Should Match",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                etVerifyPassword.setError("Passwords Should match ");
+            }
+
+
+
+
         }
         catch(Exception e)
         {
